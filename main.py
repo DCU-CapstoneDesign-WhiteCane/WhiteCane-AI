3495f8f955faa2f53c34746c07f77b82f3035b8a
ChoiBoHyeon <cb7856@naver.com>
2023-02-23 17:41:08 +0900

    []

--- main.py
+++ main.py
@@ -25,14 +25,16 @@ path = r'C:\Users\kgd39\PycharmProjects\AI-Test\converted_keras\keras_model.h5'
 model_filename = path
 model = tensorflow.keras.models.load_model(model_filename)
 
-# 카메라 캡쳐 객체, 0=내장 카메라
+# 카메라 캡쳐 객체, 0 = 내장 카메라
 capture = cv2.VideoCapture(0)
 
 # 캡쳐 프레임 사이즈 조절
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 
-sleep_cnt = 1  # 3초간 제품을 인식할 변수
+sleep_cnt = 0  # 3초간 제품을 인식할 변수
+
+product_check = "Null" # 객체 인식하는 상품 이름 저장할 변수
 while True:
     ret, frame = capture.read()
     if ret == True:
@@ -54,60 +56,73 @@ while True:
     # 예측
     prediction = model.predict(preprocessed)
     # print(prediction) # [[0.00533728 0.99466264]]
+    """
     print(prediction)
     print(prediction[0, 0])
     print(prediction[0, 1])
     print(prediction[0, 2])
     print(prediction[0, 3])
     print(prediction[0, 4])
+    """
 
+    # 1초 정지
     time.sleep(1)
 
     if prediction[0, 0] > 0.94:
-        print('(코카콜라)인식중')
-        sleep_cnt += 1
-        print(prediction[0, 0])
-
-        # 졸린 상태가 30초간 지속되면 소리 & 카카오톡 보내기
-        if sleep_cnt / 3 == 0:
-            sleep_cnt = 1
-            print('코카콜라')
-            break  ## 1번만 알람이 오면 프로그램을 정지 시킴 (반복을 원한다면, 주석으로 막기!)
+        print('제품이 잘 보이지 않습니다.')
+        product_check = "Null"
+        sleep_cnt = 0
     elif prediction[0, 1] > 0.94:
         print('제품이 잘 보이지 않습니다.')
-        sleep_cnt = 1
+        product_check = "Null"
+        sleep_cnt = 0
 
     elif prediction[0, 2] > 0.94:
-        print('(팹시)인식중')
-        sleep_cnt += 1
-        print(prediction[0, 2])
+        print('제품이 잘 보이지 않습니다.')
+        product_check = "Null"
+        sleep_cnt = 0
 
-        # 졸린 상태가 30초간 지속되면 소리 & 카카오톡 보내기
-        if sleep_cnt / 3 == 0:
-            sleep_cnt = 1
-            print('펩시')
-            break  ## 1번만 알람이 오면 프로그램을 정지 시킴 (반복을 원한다면, 주석으로 막기!)
     elif prediction[0, 3] > 0.94:
-        print('(스프라이트)인식중')
+        product_name = "코카콜라"
+        print('(' + product_name + ')인식중')
         sleep_cnt += 1
+        print(prediction[0, 3])
+        if product_name != product_check:
+            product_check = product_name
+            sleep_cnt = 0
 
-        # 졸린 상태가 30초간 지속되면 소리 & 카카오톡 보내기
-        if sleep_cnt / 3 == 0:
-            sleep_cnt = 1
+        if sleep_cnt == 4:
+            #sleep_cnt = 1
+            print('코카콜라')
+            break  ## 1번만 알람이 오면 프로그램을 정지 시킴 (반복을 원한다면, 주석으로 막기!)
+
+    elif prediction[0, 4] > 0.94:
+        product_name = "스프라이트"
+        print('('+ product_name +')인식중')
+        sleep_cnt += 1
+        print(prediction[0, 4])
+        if product_name != product_check:
+            product_check == product_name
+            sleep_cnt = 0
+
+        if sleep_cnt == 4:
+            #sleep_cnt = 1
             print('스프라이트')
             break  ## 1번만 알람이 오면 프로그램을 정지 시킴 (반복을 원한다면, 주석으로 막기!)
-    elif prediction[0, 4] > 0.94:
-        print('(베리베리스트로베리)인식중')
-        sleep_cnt += 1
 
-        # 졸린 상태가 30초간 지속되면 소리 & 카카오톡 보내기
-        if sleep_cnt / 3 == 0:
-            sleep_cnt = 1
-            print('베리베리스트로베리')
-            break  ## 1번만 알람이 오면 프로그램을 정지 시킴 (반복을 원한다면, 주석으로 막기!)
     else:
         print('제품을 인식할 수 없습니다.')
         sleep_cnt = 1
+
+    """elif prediction[0, 4] > 0.94:
+        print('(팹시)인식중')
+        sleep_cnt += 1
+
+        if sleep_cnt / 3 == 0:
+            sleep_cnt = 1
+            print('팹시')
+            break  ## 1번만 알람이 오면 프로그램을 정지 시킴 (반복을 원한다면, 주석으로 막기!)
+    """
 
 # 카메라 객체 반환
 capture.release()
