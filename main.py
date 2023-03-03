97834030a49e6e04f05c81bc93ab1b48f94269f2
ChoiBoHyeon <cb7856@naver.com>
2023-03-03 22:29:26 +0900

    폴더 형식의 Keras 모델로 구현

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import logging
+
 import cv2
 import tensorflow.python.keras
 import numpy as np
@@ -20,10 +22,20 @@ def preprocessing(frame):
     return frame_reshaped
 
 
-## 학습된 모델 불러오기
-path = r'C:\Users\kgd39\PycharmProjects\AI-Test\converted_keras\keras_model.h5'
-model_filename = path
-model = tensorflow.keras.models.load_model(model_filename)
+## 학습된 FORM 모델 불러오기
+form_path = r'C:\Users\kgd39\PycharmProjects\AI-Test\converted_keras\converted_keras(FORM)\keras_model.h5'
+form_model_filename = form_path
+form_model = tensorflow.keras.models.load_model(form_model_filename)
+
+## 학습된 CAN 모델 불러오기
+can_path = r'C:\Users\kgd39\PycharmProjects\AI-Test\converted_keras\converted_keras(CAN)\keras_model.h5'
+can_model_filename = can_path
+can_model = tensorflow.keras.models.load_model(can_model_filename)
+
+## 학습된 PET 모델 불러오기
+pet_path = r'C:\Users\kgd39\PycharmProjects\AI-Test\converted_keras\converted_keras(PET)\keras_model.h5'
+pet_model_filename = pet_path
+pet_model = tensorflow.keras.models.load_model(pet_model_filename)
 
 # 카메라 캡쳐 객체, 0 = 내장 카메라
 capture = cv2.VideoCapture(0)
@@ -32,9 +44,11 @@ capture = cv2.VideoCapture(0)
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 
-sleep_cnt = 0  # 3초간 제품을 인식할 변수
+form_cnt = 0  # 3초간 제품을 인식할 변수
+sleep_cnt = 0
 
 product_check = "Null" # 객체 인식하는 상품 이름 저장할 변수
+form_check = "Null"
 while True:
     ret, frame = capture.read()
     if ret == True:
@@ -54,75 +68,174 @@ while True:
     preprocessed = preprocessing(frame)
 
     # 예측
-    prediction = model.predict(preprocessed)
+    prediction_form = form_model.predict(preprocessed)
+    prediction_can = can_model.predict(preprocessed)
+    prediction_pet = pet_model.predict(preprocessed)
     # print(prediction) # [[0.00533728 0.99466264]]
+
     """
     print(prediction)
-    print(prediction[0, 0])
-    print(prediction[0, 1])
-    print(prediction[0, 2])
-    print(prediction[0, 3])
-    print(prediction[0, 4])
+    print(prediction[0, 0]) #None1
+    print(prediction[0, 1]) #None2
+    print(prediction[0, 3]) #Coca
+    print(prediction[0, 4]) #Sprite
     """
 
     # 1초 정지
     time.sleep(1)
-
-    if prediction[0, 0] > 0.94:
+    # 형태 인식
+    if prediction_form[0, 0] > 0.94:
         print('제품이 잘 보이지 않습니다.')
         product_check = "Null"
-        sleep_cnt = 0
-    elif prediction[0, 1] > 0.94:
+        form_cnt = 0
+        print(form_cnt)
+
+    elif prediction_form[0, 1] > 0.94:
         print('제품이 잘 보이지 않습니다.')
         product_check = "Null"
-        sleep_cnt = 0
+        form_cnt = 0
+        print(form_cnt)
 
-    elif prediction[0, 2] > 0.94:
-        print('제품이 잘 보이지 않습니다.')
-        product_check = "Null"
-        sleep_cnt = 0
+    # 캔으로 판별
+    elif prediction_form[0, 2] > 0.94:
+        form_name = "캔음류"
+        print('(' + form_name + ')인식중')
+        form_cnt += 1
+        print(prediction_form[0, 2])
+        if form_name != form_check:
+            form_check = form_name
+            form_cnt = 0
 
-    elif prediction[0, 3] > 0.94:
-        product_name = "코카콜라"
-        print('(' + product_name + ')인식중')
-        sleep_cnt += 1
-        print(prediction[0, 3])
-        if product_name != product_check:
-            product_check = product_name
-            sleep_cnt = 0
+        if form_cnt >= 3:
+            print(form_name + "으로 인식")
+            form_cnt = 1
 
-        if sleep_cnt == 4:
-            #sleep_cnt = 1
-            print('코카콜라')
-            break  ## 1번만 알람이 오면 프로그램을 정지 시킴 (반복을 원한다면, 주석으로 막기!)
+            if prediction_can[0, 0] > 0.94:
+                print('제품이 잘 보이지 않습니다.')
+                product_check = "Null"
+                sleep_cnt = 0
 
-    elif prediction[0, 4] > 0.94:
-        product_name = "스프라이트"
-        print('('+ product_name +')인식중')
-        sleep_cnt += 1
-        print(prediction[0, 4])
-        if product_name != product_check:
-            product_check == product_name
-            sleep_cnt = 0
+            elif prediction_can[0, 1] > 0.94:
+                print('제품이 잘 보이지 않습니다.')
+                product_check = "Null"
+                sleep_cnt = 0
 
-        if sleep_cnt == 4:
-            #sleep_cnt = 1
-            print('스프라이트')
-            break  ## 1번만 알람이 오면 프로그램을 정지 시킴 (반복을 원한다면, 주석으로 막기!)
+            elif prediction_can[0, 2] > 0.94:
+                product_name = "스프라이트"
+                print('(' + product_name + ') 캔 인식중')
+                sleep_cnt += 1
+                print(prediction_can[0, 2])
+
+                if sleep_cnt == 4:
+                    # sleep_cnt = 1
+                    print('스프라이트 캔')
+                    break
+
+            elif prediction_can[0, 3] > 0.94:
+                product_name = "코카콜라"
+                print('(' + product_name + ') 캔 인식중')
+                sleep_cnt += 1
+                print(prediction_can[0, 3])
+
+                if sleep_cnt == 4:
+                    # sleep_cnt = 1
+                    print('코카콜라 캔')
+                    break
+
+            elif prediction_can[0, 4] > 0.94:
+                product_name = "펩시"
+                print('(' + product_name + ') 캔 인식중')
+                sleep_cnt += 1
+                print(prediction_can[0, 4])
+
+
+                if sleep_cnt == 4:
+                    # sleep_cnt = 1
+                    print('펩시 캔')
+                    break
+
+            else:
+                print('제품을 인식할 수 없습니다.')
+                sleep_cnt = 1
+
+    # 페트병으로 판별
+    elif prediction_form[0, 3] > 0.94:
+        form_name = "페트병음류"
+        print('(' + form_name + ')인식중')
+        form_cnt += 1
+        print(prediction_form[0, 3])
+        print(form_cnt)
+        if form_name != form_check:
+            form_check = form_name
+            form_cnt = 0
+
+        if form_cnt >= 3:
+            print(form_name + "으로 인식")
+            form_cnt = 1
+
+            if prediction_pet[0, 0] > 0.94:
+                print('제품이 잘 보이지 않습니다.')
+                product_check = "Null"
+                sleep_cnt = 0
+
+            elif prediction_pet[0, 1] > 0.94:
+                print('제품이 잘 보이지 않습니다.')
+                product_check = "Null"
+                sleep_cnt = 0
+
+            elif prediction_pet[0, 2] > 0.94:
+                product_name = "스프라이트"
+                print('(' + product_name + ') 페트병 인식중')
+                sleep_cnt += 1
+                print(prediction_can[0, 2])
+
+                if product_name != product_check:
+                    product_check = product_name
+                    sleep_cnt = 0
+
+                if sleep_cnt == 4:
+                    # sleep_cnt = 1
+                    print('스프라이트 페트병')
+                    break
+
+            elif prediction_pet[0, 3] > 0.94:
+                product_name = "코카콜라"
+                print('(' + product_name + ') 페트병 인식중')
+                sleep_cnt += 1
+                print(prediction_can[0, 3])
+
+                if product_name != product_check:
+                    product_check = product_name
+                    sleep_cnt = 0
+
+                if sleep_cnt == 4:
+                    # sleep_cnt = 1
+                    print('코카콜라 페트병')
+                    break
+
+            elif prediction_pet[0, 4] > 0.94:
+                product_name = "펩시"
+                print('(' + product_name + ') 페트병 인식중')
+                sleep_cnt += 1
+                print(prediction_can[0, 4])
+
+                if product_name != product_check:
+                    product_check = product_name
+                    sleep_cnt = 0
+
+                if sleep_cnt == 4:
+                    # sleep_cnt = 1
+                    print('펩시 페트병')
+                    break
+
+            else:
+                print('제품을 인식할 수 없습니다.')
+                sleep_cnt = 1
 
     else:
         print('제품을 인식할 수 없습니다.')
-        sleep_cnt = 1
+        form_cnt = 0
 
-    """elif prediction[0, 4] > 0.94:
-        print('(팹시)인식중')
-        sleep_cnt += 1
-
-        if sleep_cnt / 3 == 0:
-            sleep_cnt = 1
-            print('팹시')
-            break  ## 1번만 알람이 오면 프로그램을 정지 시킴 (반복을 원한다면, 주석으로 막기!)
-    """
 
 # 카메라 객체 반환
 capture.release()
